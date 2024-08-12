5e4b8c09d4e58efc4d75c6a05bb46a85edf17b68
JakAsAlways <mrjak366@gmail.com>
2024-08-12 19:37:36 +0400

    Finished the development of a simple todo list website

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export default function TodoCard() {
-  return (
-    <div>
-      TodoCard
-    </div>
-  )
+export default function TodoCard(props) {
+    const { children, handleDeleteTodo, index, handleEditTodo } = props
+    return (
+        <li className='todoItem'>
+            {children}
+            <div className="actions-container">
+                <button onClick={() => {
+                    handleEditTodo(index)
+                }}>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button onClick={() => {
+                    handleDeleteTodo(index)
+                }}>
+                    <i className="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        </li>
+    )
 }
